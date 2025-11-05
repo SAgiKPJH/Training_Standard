bae4ed04e86395137af7921d786a6db41ae79f40
SagiKPJH1021 <Juhyung1021@gmail.com>
2025-11-05 16:48:33 +0900

    feat: add json hyperparameter builder

---
+++ Training Standard/Builder/HyperParameter/Json_HyperParameterBuilder.py
@@ -0,0 +1,89 @@
+import json
+
+parameters = '''{
+    "hyperparameter":{
+        "epoch" : 20,
+        "save_epoch" : 1,
+        "batch_size" : 16,
+        "lr" : 1e-3,
+        "optimizer_name" : "Adam",
+        "input_size" : 299,
+        "normalize_mean" : 0.5,
+        "normalize_stdev" : 0.5,
+        "using_gpu" : true,
+        "using_amp" : true,
+        "train_ratio" : 0.8,
+        "validation_save_random" : false
+    }
+}'''
+
+class Json_HyperparameterBuilder:
+    def __init__(self, json_parameter = parameters):
+        self.__hyperparams = json.loads(json_parameter)
+
+    def initialize(self):
+        self.__hyperparams['epoch'] = int(self.__hyperparams['epoch'])
+        self.__hyperparams['batch_size'] = int(self.__hyperparams['batch_size'])
+        return self
+
+    def get_train_ratio(self):
+        return self.__hyperparams['train_ratio']
+    
+    def get_batch_size(self):
+        return self.__hyperparams['batch_size']
+    
+    def get_validation_save_random(self):
+        return self.__hyperparams.get('validation_save_random', False)
+    
+    def get_optimizer(self):
+        keys = ['optimizer', 'optimizer_name']
+        for key in keys:
+            if key in self.__hyperparams:
+                return self.__hyperparams[key]
+
+        raise KeyError(f"Optimizer not found. Tried keys: {', '.join(keys)}")
+    
+    def get_learning_rate(self):
+        keys = ['lr', 'learningRate', 'LearningRate', 'Learningrate']
+        for key in keys:
+            if key in self.__hyperparams:
+                return self.__hyperparams[key]
+
+        raise KeyError(f"Learning rate not found. Tried keys: {', '.join(keys)}")
+    
+    def get_input_size(self):
+        return self.__hyperparams['input_size']
+    
+    def get_epoch(self):
+        return self.__hyperparams['epoch']
+    
+    def get_save_epoch(self):
+        return self.__hyperparams['save_epoch']
+    
+    def get_using_amp(self):
+        return self.__hyperparams['using_amp']
+    
+    def get_early_stop_patience(self):
+        keys = ['earlyStopPatience']
+        for key in keys:
+            if key in self.__hyperparams:
+                return self.__hyperparams[key]
+        return 0
+    
+    def get_reduce_learning_rate_patience(self):
+        keys = ['reduceLRPatience']
+        for key in keys:
+            if key in self.__hyperparams:
+                return self.__hyperparams[key]
+        return 0
+
+    def get_criterion(self):
+        keys = ['criterion', 'loss']
+        for key in keys:
+            if key in self.__hyperparams:
+                return self.__hyperparams[key]
+            
+        return 'CrossEntropyLoss'
+
+    def build(self):
+        return self
--- Training Standard/Builder/__init__.py
+++ Training Standard/Builder/__init__.py
@@ -1,5 +1,7 @@
 from .Save.DAQ_Savebuilder import DAQ_SaveBuilder
+from .HyperParameter.Json_HyperParameterBuilder import Json_HyperparameterBuilder
 
 __all__ = [
     'DAQ_SaveBuilder',
+    'Json_HyperparameterBuilder',
 ]
