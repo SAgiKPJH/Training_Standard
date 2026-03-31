e9a1ba9ab564dc1b7401b10b9ab564c687e5b9d6
박주형 <juhyung.park@mirero.co.kr>
2026-03-31 11:53:14 +0900

    refactor: update code

--- Training Standard/Pytorch_CNN_Standard/Builder/Operation/DAQ_OperationBuilder.py
+++ Training Standard/Pytorch_CNN_Standard/Builder/Operation/DAQ_OperationBuilder.py
@@ -6,6 +6,17 @@ from urllib.parse import urlparse
 from mpp.daq import protos
 from google.protobuf.wrappers_pb2 import StringValue
 
+DEFAULT_COMPRESSION = grpc.Compression.Gzip
+DEFAULT_GRPC_OPTIONS = [
+    ("grpc.max_send_message_length", 2147483647),
+    ("grpc.max_receive_message_length", 2147483647),
+    ("grpc.keepalive_time_ms", 30_000),
+    ("grpc.keepalive_timeout_ms", 30_000),
+    ("grpc.keepalive_permit_without_calls", 1),
+    ("grpc.http2.max_pings_without_data", 0),
+    ("grpc.http2.min_ping_interval_without_data_ms", 5_000)
+]
+
 class Operation_Builder:
     def __init__(self, **keyword_arguments):
         self.__keyword_arguments = keyword_arguments
@@ -27,7 +38,9 @@ class Operation_Builder:
         selected_address = random.choice(address.split(","))
         if not address or not selected_address:
             raise Exception("Address is required")
-        self.__operation_channel = grpc.insecure_channel(selected_address)
+        self.__operation_channel = grpc.insecure_channel(selected_address,
+                                                         options=DEFAULT_GRPC_OPTIONS,
+                                                         compression=DEFAULT_COMPRESSION)
     
     def __init_access_token(self):
         access_token = self.__keyword_arguments['authentication']['access_token']
